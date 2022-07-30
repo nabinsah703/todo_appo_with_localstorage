9c8efc545d943f9343da80ebce3fd1b937c16d31
nabin sah <nabinsah703@gmail.com>
2022-07-30 15:40:52 +0545

    localstorage todo app is complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,159 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'localstorage todo app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TodoItem {
+  String title;
+  bool done;
+  TodoItem({required this.title, required this.done});
+  toJSONEncodable() {
+    Map<String, dynamic> mapitem = Map();
+    mapitem['title'] = title;
+    mapitem['done'] = done;
+    return mapitem;
+  }
+}
 
-  void _incrementCounter() {
+class TodoList {
+  List<TodoItem> items = [];
+  toJSONEncodable() {
+    return items.map((item) {
+      return item.toJSONEncodable();
+    }).toList();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  final TodoList list = TodoList();
+  final LocalStorage storage = LocalStorage('todo_app');
+  bool initialized = false;
+  TextEditingController controller = TextEditingController();
+
+  _toggleItem(TodoItem item) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      item.done = !item.done;
+      _saveToStorage();
+    });
+  }
+
+  _addItem(String title) {
+    setState(() {
+      final item = TodoItem(title: title, done: false);
+      list.items.add(item);
+      _saveToStorage();
+    });
+  }
+
+  _saveToStorage() {
+    storage.setItem('todos', list.toJSONEncodable());
+  }
+
+  _clearStorage() async {
+    await storage.clear();
+    setState(() {
+      list.items = storage.getItem('todos') ?? [];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Localstorage demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        constraints: const BoxConstraints.expand(),
+        child: FutureBuilder(
+          future: storage.ready,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (!initialized) {
+              var items = storage.getItem('todos');
+              if (items != null) {
+                list.items = List<TodoItem>.from(
+                  (items as List).map(
+                    (item) => TodoItem(
+                      title: item['title'],
+                      done: item['done'],
+                    ),
+                  ),
+                );
+              }
+              initialized = true;
+            }
+
+            List<Widget> widgets = list.items.map((item) {
+              return CheckboxListTile(
+                value: item.done,
+                title: Text(item.title),
+                selected: item.done,
+                onChanged: (_) {
+                  _toggleItem(item);
+                },
+              );
+            }).toList();
+            return Column(children: [
+              Expanded(
+                flex: 1,
+                child: ListView(
+                  children: widgets,
+                  itemExtent: 50.0,
+                ),
+              ),
+              ListTile(
+                title: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    labelText: 'What to do ?',
+                  ),
+                  onEditingComplete: _save,
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.save),
+                      onPressed: _save,
+                      tooltip: 'Save',
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: _clearStorage,
+                      tooltip: 'Clear storage',
+                    )
+                  ],
+                ),
+              )
+            ]);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _save() {
+    _addItem(controller.value.text);
+    controller.clear();
   }
 }
